6086d65bfe35b6257f96b4f4744b7840df75b155
Gilles Goudmaeker <gilles@goudmaeker.be>
2021-05-14 13:14:56 +0200

    ajout de l'ajout dans le groupe principal de l'utilisateur. .

--- CreateUser.ps1
+++ CreateUser.ps1
@@ -1,6 +1,4 @@
-﻿$errorLogs = "c:\temp\%date% +CreateUser.log"
-
-
+﻿
 function SelectFichierSource {
 #fonction qui Ouvre l'explorateur windows pour permettre à l'utilisateur de sélectionner le fichier source.
 Write-host "Sélectionnez le fichier qui contient les paramètres utilisateur."
@@ -12,26 +10,66 @@ $FileBrowser.FileName}
 
 function AliasMail {
 #Vérifie si l'utilisateur à des alias mail et construit la variable $Global:Proxyaddresses qui sera utilisée pendant la création du compte
+$Email="SMTP:"+$i.email
 $Alias1=$i.alias1
 $Alias2=$i.alias2
 $Alias3=$i.alias3
 $Alias4=$i.alias4
-$hash=@( $Alias1 , $Alias2 , $Alias3 , $Alias4)
-$AliasMail = $hash |where{$_ -ne ""} 
-$global:proxyaddresses=$AliasMail -join ','
+$ProxyAddresses= $Email
+if ($ALias1 -ne "") { $ProxyAddresses+= ' '+','+"smtp:"+"$Alias1" }
+if ($ALias2 -ne "") { $ProxyAddresses+= ' '+','+"smtp:"+"$Alias2" }
+if ($ALias3 -ne "") { $ProxyAddresses+= ' '+','+"smtp:"+"$Alias3" }
+if ($ALias4 -ne "") { $ProxyAddresses+= ' '+','+"smtp:"+"$Alias4" }
+$global:proxyaddresses=@($ProxyAddresses)
 }
+
 Function UPN {
 #crée le paramètre UserPrincipalName sur base de l'émail de l'employé
 $Domain = $null
 if ($i.email -ne "" ) 
-    { $domain = $i.email}
+    { $domain = "acme.fr"}
     else {$:domain = "acme.local"}
-    
-$Global:UserPrincipalName=$SamaccountName+"@"+$DOMAIN
+       
+$Global:UserPrincipalName=$SamaccountName+"@"+$DOMAIN 
 }
 
+Function AddGroup {
+#Ajoute l'utilisateur au groupe principal de son département. 
+$Service=$i.Département
+switch  ($service)
+    {
+    "Direction Générale" { $Group ="AG-DirectionGenerale"}
+    "Direction Marketing" {$Group = "AG-DirectionMarketing"}
+    "Direction Technique" { $Group = "AG-DirectionTechnique" }
+    "Direction Financière" { $Group = "AG-DirectionFinanciere"}
+    "Ressources Humaines" { $Group = "AG-RessourcesHumaines"}
+    }
+    Add-ADGroupmember -identity $Group -Members $SamaccountName
+}
+    
 
-$Source=open-file
+Function CreateAdAccount {
+#creation du compte utilisateur
+$password= ConvertTo-SecureString -String Pwd2021+ -AsPlainText -Force
+try {
+New-aduser -SamAccountName $samaccountName -UserPrincipalName $global:userprincipalName -path $path -GivenName $GivenName -SurName $LastName -Name $DisplayName -DisplayName $DisplayName `
+-title $title -EmailAddress $i.email -ChangePasswordAtLogon $true -Enabled $true -AccountPassword $password `
+ -city $i.site -Company $i.domaine -Country FR  -Department $department -MobilePhone $i.Mobile -OfficePhone $i.Téléphone -office $i.site `
+  -OtherAttributes @{'proxyaddresses'=$global:proxyaddresses} -ErrorAction stop 
+
+}
+catch {
+    #en cas d'erreur, on vérifie si le compte existe déjà. 
+    $CheckAccount=get-aduser -identity $samaccountname 
+    if ($CheckAccount -ne $null){
+    write-output "Le compet $samaccountname existe dans l'AD"
+    break
+    }
+    }
+}
+
+start-transcript -path "c:\temp\$(get-date -f yy-mm-dd)-CreateUser.txt"
+$Source=SelectFichierSource
 
 #Import du fichier sources et boucle sur le contenu du tableau et on assigne les colonnes aux variables qui seront utilisées. 
 $table=import-csv $Source -Encoding "utf8" -delimiter ";"
@@ -39,15 +77,20 @@ $table=import-csv $Source -Encoding "utf8" -delimiter ";"
 foreach ($i in $table) {
 $GivenName= $i.prénom
 $LastName = $i.Nom
-$DisplayName = $GivenName +' '+ $LastName
+$DisplayName =$GivenName +' '+ $LastName 
 #Le nom d'utilisateur est généré sur base du Prénom et du nom. Seule la 1ère lettre du prénom est utilisée
 #pour créer le nom d'utilisateur
 $SamAccountName = ("$GivenName").Substring(0,1)+$LastName
+$title=$i.Fonction
+$Department= $i.Département
+$site= $i.Site
+$path= "OU=$Department,OU=$site,DC=LABO,DC=LOCAL"
+
 
 
 UPN
 AliasMail
-write-host $LastName $global:UserPrincipalName $global:proxyaddresses
-
-
-}
+CreateAdAccount
+AddGroup
+    }
+Stop-Transcript
