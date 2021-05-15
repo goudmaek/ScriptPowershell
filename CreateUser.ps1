1fe6ad8b40fec067c17d504a88f01ecf519dc684
Gilles Goudmaeker <gilles@goudmaeker.be>
2021-05-15 04:35:52 +0200

    ajout des retours en console pendant l'exécution du script. ajout de la gestion des droit sur dossier personnel.

--- CreateUser.ps1
+++ CreateUser.ps1
@@ -45,6 +45,7 @@ switch  ($service)
     "Ressources Humaines" { $Group = "AG-RessourcesHumaines"}
     }
     Add-ADGroupmember -identity $Group -Members $SamaccountName
+    write-output "Ajout du compte $SamAccountName de $DisplayName dans le groupe $Group"
 }
     
 
@@ -56,17 +57,46 @@ New-aduser -SamAccountName $samaccountName -UserPrincipalName $global:userprinci
 -title $title -EmailAddress $i.email -ChangePasswordAtLogon $true -Enabled $true -AccountPassword $password `
  -city $i.site -Company $i.domaine -Country FR  -Department $department -MobilePhone $i.Mobile -OfficePhone $i.Téléphone -office $i.site `
   -OtherAttributes @{'proxyaddresses'=$global:proxyaddresses} -ErrorAction stop 
+  write-Output "Compte -$DisplayName avec le login $SamAccountName  et le mot de passe $password créé"
 
 }
 catch {
     #en cas d'erreur, on vérifie si le compte existe déjà. 
     $CheckAccount=get-aduser -identity $samaccountname 
     if ($CheckAccount -ne $null){
-    write-output "Le compet $samaccountname existe dans l'AD"
+    write-output "Le compte $samaccountname existe dans l'AD"
     break
     }
     }
 }
+
+function CreateFolder {
+    $server="lab-dc01.labo.local"
+    $SharedFolder="Homes"
+    $Global:SharesPath="\\$server" + "\$SharedFolder" + "\$SamAccountName"
+    write-host $SamAccountName and $SharesPath
+    $TestFolder=test-path $SharesPath
+    if($TestFolder -eq $false) {
+    New-item -path $sharesPath -ItemType directory
+    write-Output "Création du dossier personnel $SamAccountName de $DisplayName"
+    
+    }
+    else { 
+        write-host "le dossier existe"
+        }
+}
+
+function SetAcl {
+    $ACL = Get-ACL -Path $Global:SharesPath
+    $identity = $SamAccountName
+    $filesystemRights = "Modify","delete","DeleteSubdirectoriesAndFiles"
+    $type = "allow"
+    $filesystemAccessRuleArgumentList = $identity, $filesystemRights, $type
+    $filesystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $filesystemAccessRuleArgumentList 
+    $Acl.SetAccessRule($filesystemAccessRule)
+    set-acl -path "$sharespath" -AclObject $ACL
+    }
+
 
 start-transcript -path "c:\temp\$(get-date -f yy-mm-dd)-CreateUser.txt"
 $Source=SelectFichierSource
@@ -92,5 +122,7 @@ UPN
 AliasMail
 CreateAdAccount
 AddGroup
-    }
+CreateFolder
+setacl
+ }
 Stop-Transcript
