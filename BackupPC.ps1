2fb0962478dd1da9a3801bf580d4655087372923
Gilles Goudmaeker <gilles@goudmaeker.be>
2021-05-17 19:42:35 +0200

    creation de la routine de vérification des ordinateurs

---
+++ BackupPC.ps1
@@ -0,0 +1,17 @@
+﻿$PC=get-adgroupmember -identity ag-ordinateursasauvegarder
+#write-host $PC
+foreach ($computer in $pc) {
+$NomOrdinateur=$computer.name
+write-host $NomOrdinateur
+$path = "\\$nomordinateur\c$\users"
+#dir $path
+write-host $path
+test-path -path  $path
+if((test-path -path $path) -eq "$true") {
+write-host "accès OK"
+}
+else {
+write-host "Accès NOK"
+}
+
+}
